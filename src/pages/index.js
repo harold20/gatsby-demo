0cde65163b3af0e6ba2867064b26e5565c4fed91
Harold <hjuarez20@gmail.com>
2018-11-12 13:16:13 -0600

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>JUZGADO DECLARA ILEGAL HUELGA EN EL MEP</h1>
+      <h3>Sindicatos condenados a pagar ¢6 millones por costas;
+          Ministerio llama a funcionarios a retomar labores para reactivar curso lectivo</h3>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+      <p>Luego de 64 días de huelga, se declara ilegal la huelga del MEP, este viernes 16 de noviembre el juzgado dio su declaratoria; se condena al Sindicato de Educadores Costarricenses (SEC), Asociación Nacional de Educadores (ANDE), y Asociación de Profesores de Segunda Enseñanza (APSE) al pago de ¢1 millón por costas procesales y ¢5 millones por costas personales (pago de abogados).</p>
+      <p>Y es que no es casualidad que se condene este tipo de acciones aún fuera de nuestra legislación, dado que si bien es cierto las manifestaciones son la expresión viva del movimiento de huelga, estas no deben sobrepasar los límites de racionalidad y proporcionalidad del actuar de cualquier gremio.</p>
+      <p>Luego de darse a conocer la declaratoria de ilegalidad, las autoridades educativas llamaron a los funcionarios a retomar a labores.</p>
+      <p>“Desde el MEP llamamos a todo el personal docente, administrativo, conserjes, cocineras y guardas a retomar sus labores de forma inmediata para reactivar el curso lectivo 2018”, dijo el Ministerio de Educación esta mañana mediante redes sociales.</p>
+      <p>En total fueron 32 las peticiones de declaratoria de ilegalidad que hicieron las instituciones públicas desde el inicio del movimiento. Hasta la fecha, 19 han sido declaradas ilegales, incluyendo la del MEP, dos más fueron calificadas como legales.</p>
+      <p>Las consecuencias desastrosas de esta huelga ilegal e injustificada han dejado, al menos 79.000 niños y adolescentes han dejado de recibir alimentación por el cierre de comedores escolares.</p>
+  </Layout>
+)
+
+export default IndexPage
