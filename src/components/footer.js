1c72cc5d2e3bb318942f018f862a66ceb15c85e7
Harold <hjuarez20@gmail.com>
2018-11-12 14:03:34 -0600

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Footer = ({ siteTitle }) => (
+    <div
+        style={{
+            background: 'rebeccapurple',
+        }}
+    >
+        <div
+            style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '1.45rem 1.0875rem',
+                color: 'white'
+            }}
+        >
+            <p> Mundo Informativo CR. Todos los derechos reservados © 2018.</p>
+        </div>
+    </div>
+)
+
+export default Footer
